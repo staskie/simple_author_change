b2e40055aa6be90d94618aa74925cdc1e3729770
Dominik Staskiewicz <stadominik@gmail.com>
2015-01-31 22:53:47 +0100

    Display correct log details for authors

--- lib/simple_author_change/hooks.rb
+++ lib/simple_author_change/hooks.rb
@@ -1,5 +1,18 @@
 module SimpleAuthorChange
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_issues_form_details_top, :partial => 'issues/simple_author_change'
+
+    def helper_issues_show_detail_after_setting(context = {})
+      detail = context[:detail]
+
+      case detail.property
+      when 'attr'
+        if detail.prop_key == 'author_id'
+          detail[:value] = User.find detail.value 
+          detail[:old_value] = User.find detail.old_value
+        end
+      end
+    end
+
   end
 end
