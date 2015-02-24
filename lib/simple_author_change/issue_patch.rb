405085e0833fe8e19f788136977b5997b16f4528
Dominik Staskiewicz <stadominik@gmail.com>
2015-02-24 21:55:15 +0100

    Add anonymous user to potential authors

--- lib/simple_author_change/issue_patch.rb
+++ lib/simple_author_change/issue_patch.rb
@@ -6,6 +6,15 @@ module SimpleAuthorChange
       base.class_eval do
         safe_attributes :author_id
       end
+
+      base.send(:include, InstanceMethods)
+    end
+
+    module InstanceMethods
+      def potential_authors
+        (project.members.map(&:user) + [User.anonymous]).
+          uniq.select { |user| user.allowed_to?(:add_issues, project)}
+      end
     end
   end
 end
