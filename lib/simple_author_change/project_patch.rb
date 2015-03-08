b083f08c265c90f34b86b67fc361a457585eccae
Dominik Staskiewicz <stadominik@gmail.com>
2015-03-08 22:07:22 +0100

    Performance fix - require at least 3 characters to start searching

--- lib/simple_author_change/project_patch.rb
+++ lib/simple_author_change/project_patch.rb
@@ -7,19 +7,27 @@ module SimpleAuthorChange
     end
 
     module InstanceMethods
+      MinQueryLength = 3
+
       def potential_authors(query)
-        # Limit the number of queries required to work out members of a project (including anonymous user)
-        # that are permitted to create issues for a given project and return ActiveModel::Relation
-        roles = Role.all.select { |r| r.has_permission?(:add_issues) }
-        roles_user_ids = roles.map { |role| role.members.includes(:user) }.flatten.map(&:user).uniq.compact.map(&:id)
+        # Trigger search only when having more than 2 characters for a query
+        if query.length >= MinQueryLength
+          # Limit the number of queries required to work out members of a project (including anonymous user)
+          # that are permitted to create issues for a given project and return ActiveModel::Relation
+          role_ids           = Role.all.select { |r| r.has_permission?(:add_issues) }.map(&:id)
+          member_ids         = Member.includes(:roles).where('roles.id in (?)', role_ids).map(&:id)
+          roles_user_ids     = Member.includes(:user).where(id: member_ids).map(&:user).compact.map(&:id)
+          permitted_user_ids = users.where(id: roles_user_ids).map(&:id)
 
-        permitted_user_ids = users.where(id: roles_user_ids).map(&:id)
+          if User.anonymous.allowed_to?(:add_issues, self)
+            permitted_user_ids << User.anonymous.id
+          end
 
-        if User.anonymous.allowed_to?(:add_issues, self)
-          permitted_user_ids << User.anonymous.id
+          User.where(id: permitted_user_ids).like(query).sorted
+        else
+          # Return empty ActiveRecord::Relation
+          User.where('1=2')
         end
-
-        User.where(id: permitted_user_ids).like(query).sorted
       end
     end
   end
