6d728f4adce64e8615e4bfa503cd7b67b3d35037
Dominik Staskiewicz <stadominik@gmail.com>
2016-01-22 00:07:28 +0100

    Deal with changed context

--- lib/simple_author_change/hooks.rb
+++ lib/simple_author_change/hooks.rb
@@ -5,10 +5,14 @@ module SimpleAuthorChange
     def helper_issues_show_detail_after_setting(context = {})
       detail = context[:detail]
 
+      # Hack (ugh) to deal with changed context
+      # Issue #10
+      return unless User.where(id: detail.value).first
+
       case detail.property
       when 'attr'
         if detail.prop_key == 'author_id'
-          detail[:value] = User.find detail.value 
+          detail[:value] = User.find detail.value
           detail[:old_value] = User.find detail.old_value
         end
       end
